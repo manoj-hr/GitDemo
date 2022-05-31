fb6e4aae07b6c6eabd1f53ce59ee3e1db0bf3147
manoj <manojhr1238@gmail.com>
2022-05-31 12:06:19 +0530

    third commit

--- DatabaseTesting/src/test/java/dbTesting/Base.java
+++ DatabaseTesting/src/test/java/dbTesting/Base.java
@@ -4,5 +4,9 @@ public class Base {
     public static void main(String[] args) {
         System.out.println("added new things");
         System.out.println("pull the updates before starting the work");
+
+
+        System.out.println("ok thanks for ur info");
+        System.out.println("i have also updated few things pls update and then start");
     }
 }
