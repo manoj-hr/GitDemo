cfaab7395d15e31469e85c27ff2da325947eebe6
manoj <manojhr1238@gmail.com>
2022-05-31 11:50:13 +0530

    second commit

---
+++ DatabaseTesting/src/test/java/dbTesting/Base.java
@@ -0,0 +1,8 @@
+package dbTesting;
+
+public class Base {
+    public static void main(String[] args) {
+        System.out.println("added new things");
+        System.out.println("pull the updates before starting the work");
+    }
+}
